aca21d814297b9fc51fface984c6de6bdc6e022e
kenny850917 <kenny850917@users.noreply.github.com>
2020-08-11 18:14:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const winston = require("winston");
 const express = require("express");
 const config = require("config");
+const cors = require("cors");
 const app = express();
 
 require("./startup/logging")();
 require("./startup/cors")(app);
 require("./startup/routes")(app);
 require("./startup/db")();
+require("./startup/cors")(app);
 require("./startup/config")();
 require("./startup/validation")();
 
