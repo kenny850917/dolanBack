8160f7023296122fc55b664460419a90756375ac
kenny850917 <kenny850917@users.noreply.github.com>
2020-08-11 17:35:43 -0700

    Update db.js

--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,14 @@
-const winston = require('winston');
-const mongoose = require('mongoose');
-const config = require('config');
+const winston = require("winston");
+const mongoose = require("mongoose");
+const config = require("config");
 
-module.exports = function() {
-  const db = config.get('db');
-  mongoose.connect(db)
+module.exports = function () {
+  // const db = config.get('db');
+  const db = process.env.MONGODB_URL;
+  mongoose
+    .connect(db, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    })
     .then(() => winston.info(`Connected to ${db}...`));
-}
+};
