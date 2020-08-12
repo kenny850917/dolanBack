ab72f69335d2dd840d7d13f74edfe62fe4d2a411
kenny850917 <kenny850917@users.noreply.github.com>
2020-08-11 18:08:58 -0700

    update cors

--- startup/cors.js
+++ startup/cors.js
@@ -1,5 +1,9 @@
 const cors = require("cors");
 
-module.exports = function(app) {
-  app.use(cors());
+module.exports = function (app) {
+  corsOptions = {
+    origin: "https://dolan-showcase.herokuapp.com/",
+    optionsSuccessStatus: 200,
+  };
+  app.use(cors(corsOptions));
 };
